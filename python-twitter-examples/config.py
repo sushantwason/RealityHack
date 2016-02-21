00cf6afcf51d834710689581af6b8077d558ccf2
Rahul Anand <ranand@groupon.com>
2016-02-20 16:38:59 -0800

    add new git

---
+++ python-twitter-examples/config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "cV6drNgTO6IIUii8ha4087cUW"
+consumer_secret = "kNMWzRYwSDgfXmiIVGDPWvH7OJxxWVmllIadrHP8CJWjHoqtRO"
+access_key = "51981465-qCiZJBkVPjypCQ2s0fghEYRtKlu3CH0dCDQWdgutr"
+access_secret = "WS8jbH1W0U0KwA9ZJz8FPQTAvOnXYdCrEy6OGVPOCFr9s"
